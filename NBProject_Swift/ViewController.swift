0f3d888f2a1b2e5db27e3825be609e8440b6c45c
scuser <scuser@caodongdong.local>
2017-11-14 16:14:04 +0800

    original

---
+++ NBProject_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NBProject_Swift
+//
+//  Created by scuser on 2017/11/14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
